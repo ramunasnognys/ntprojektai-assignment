301caf27a295f22d7fb57a564ca40a811e3b98bc
ramunasnognys <dev.nognys@gmail.com>
2021-03-01 19:42:42 +0100

    git first commit

---
+++ app/js/index.js
@@ -0,0 +1,16 @@
+// (bundle from remote urls)
+// import Whatever from 'https://unpkg.com/whatever'
+// import $ from "./burger-menu.js";
+// (bundle from npm)
+// import Whatever from 'whatever'
+// import $ from 'blingblingjs'
+
+// (bundle from local)
+// import whatever from './whatever.js'
+
+// postCSS processed css-4-js
+// (file as string)
+// import { default as index_css }  from './css/index.css'
+import "./burger-menu.js";
+import "./get-focusable-elements.js";
+console.log("💀");
